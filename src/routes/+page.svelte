73f86d8cac9b3197e683ed109fb67b634a75f681
Core <core@c3labs.de>
2025-04-08 00:40:15 +0200

    yeahh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit - c3labsed</h1>
+<h1>Welcome to SvelteKit - c3labsed on adapter/node</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
